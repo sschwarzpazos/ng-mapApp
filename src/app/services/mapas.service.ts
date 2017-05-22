809211ffe801f9549a78661878a53c5902dcbd7d
Santiago Schwarz <sschwarzpazos@gmail.com>
2017-05-22 12:54:24 +0200

    feat(*): Marcador interfaces added

--- src/app/services/mapas.service.ts
+++ src/app/services/mapas.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Marcador } from '../interfaces/marcador.interface';
 
 @Injectable()
 export class MapasService {
-  marcadores:any[] = [];
-  
-  constructor() { }
+  marcadores:Marcador[] = [];
+
+  constructor() {
+    let nuevoMarcador:Marcador = {
+      lat: 42.880585,
+      lng: -8.544609,
+      draggable: true,
+      titulo: 'Catedral de Santiago de Compostela'
+    };
+
+    this.marcadores.push( nuevoMarcador );
+
+  }
 
 }
